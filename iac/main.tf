c7af903ce5fc68245d9fb9a486af45ecb6d745ec
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2025-12-13 15:05:41 -0800

    Add source S3 bucket

--- iac/main.tf
+++ iac/main.tf
@@ -2,3 +2,15 @@
 provider "aws" {
     region = "us-west-2"
 }
+
+# Data source to fetch the current AWS caller identity
+data "aws_caller_identity" "current" {}
+
+# Create Source S3 Bucket
+resource "aws_s3_bucket" "source_bucket" {
+  bucket = "image-processing-source-bucket-${data.aws_caller_identity.current.account_id}"
+  tags = {
+    Project = "LambdaTrigger"
+  }
+}
+
