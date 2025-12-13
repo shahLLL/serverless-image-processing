8da9cd849fe40c48e64aebba9560f6c24035a671
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2025-12-13 15:11:23 -0800

    Add lambda execution role

--- iac/main.tf
+++ iac/main.tf
@@ -14,3 +14,22 @@ resource "aws_s3_bucket" "source_bucket" {
   }
 }
 
+# Create Lambda Execution Role
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_s3_trigger_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
