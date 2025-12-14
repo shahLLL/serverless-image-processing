61e46841e74dfca65cdba90eef81ed2b9efb2f20
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2025-12-13 16:12:24 -0800

    Add DynamoDB table with associated permissions

--- iac/main.tf
+++ iac/main.tf
@@ -44,6 +44,12 @@ resource "aws_lambda_function" "image_processor_lambda" {
   source_code_hash = filebase64sha256("lambda_function.zip")
   runtime          = "python3.9"
   timeout          = 10
+  environment {
+    variables = {
+      DYNAMODB_TABLE_NAME = aws_dynamodb_table.image_metadata_table.name
+    }
+  }
+  depends_on = [aws_iam_role_policy.lambda_dynamodb_policy]
 }
 
 # Grant S3 permission to invoke the Lambda function
@@ -67,3 +73,39 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
   depends_on = [aws_lambda_permission.allow_s3_to_invoke_lambda]
 }
+
+# Create DynamoDB Table
+resource "aws_dynamodb_table" "image_metadata_table" {
+  name           = "ImageMetadataTable"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ImageKey"
+
+  attribute {
+    name = "ImageKey"
+    type = "S"
+  }
+
+  tags = {
+    Project = "LambdaTrigger"
+  }
+}
+
+# Give Permission to Lambda to write to DynamoDB Table
+resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
+  name = "lambda_dynamodb_write_access"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "dynamodb:PutItem",
+        "dynamodb:BatchWriteItem"
+      ],
+      Resource = aws_dynamodb_table.image_metadata_table.arn
+    }]
+  })
+}
+
+
