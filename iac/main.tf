cc75e15522e75e1d1b70a3a5dfb3f66a0d3e2399
shahLLL <shahlll@yahoo.com>
2026-07-10 04:00:17 -0700

    Introduce failure

--- iac/main.tf
+++ iac/main.tf
@@ -83,7 +83,7 @@ resource "aws_lambda_function" "image_processor_lambda" {
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "lambda_function.lambda_handler"
   source_code_hash = filebase64sha256("lambda_function.zip")
-  runtime          = "python3.9"
+  runtime          = "python3.99"
   timeout          = 10
 
   environment {
